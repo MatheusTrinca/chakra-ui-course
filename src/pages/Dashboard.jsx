c90793277351214d6d65d767fe16da4ff3cdc02b
MatheusTrinca <matheusfelipetrinca@gmail.com>
2023-01-25 03:04:38 -0300

    :sparkles: feat: Dashboard

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -1,5 +1,14 @@
+import { Container, Heading, Text } from '@chakra-ui/react';
+
 export default function Dashboard() {
   return (
-    <div>Dashboard</div>
-  )
+    <Container>
+      <Heading>Chakra UI components</Heading>
+      <Text>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolorem, cum.
+      </Text>
+    </Container>
+  );
 }
+
+// #2 3:40
